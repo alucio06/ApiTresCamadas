8cea5d006eb5771dceef09410e14631bc6fbce08
Anderson <Anderson@DESKTOP-U606FOM>
2023-11-07 19:57:42 -0300

    criando interfaces de service e repository, e classes de implementação do service

---
+++ src/DevIO.Business/Interfaces/IFornecedorRepository.cs
@@ -0,0 +1,12 @@
+﻿using DevIO.Business.Models;
+
+namespace DevIO.Business.Interfaces
+{
+    public interface IFornecedorRepository : IRepository<Fornecedor>
+    {
+        Task<IEnumerable<Fornecedor>> ObterFornecedorEndereco(Guid id); 
+        Task<Fornecedor> ObterFornecedorProdutoEndereco(Guid id);
+
+        Task<Endereco> ObterEnderecoPorFornecedor(Guid fornecedorId);
+    }
+}
