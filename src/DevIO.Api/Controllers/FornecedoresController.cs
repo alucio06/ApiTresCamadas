f363a27e1435ceb279660e02928020763ce29f25
Anderson <Anderson@DESKTOP-U606FOM>
2023-11-19 17:31:58 -0300

    finalizando implementações das controllers

--- src/DevIO.Api/Controllers/FornecedoresController.cs
+++ src/DevIO.Api/Controllers/FornecedoresController.cs
@@ -1,43 +1,83 @@
-﻿using DevIO.Api.ViewModels;
+﻿using AutoMapper;
+using DevIO.Api.ViewModels;
+using DevIO.Business.Interfaces;
+using DevIO.Business.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Net;
 
 namespace DevIO.Api.Controllers
 {
     [Route("api/fornecedor")]
     public class FornecedoresController : MainController
     {
-        public FornecedoresController()
+        readonly IFornecedorRepository _fornecedorRepository;
+        readonly IFornecedorService _fornecedorService;
+        readonly IMapper _mapper;
+
+        public FornecedoresController(IFornecedorRepository fornecedorRepository, 
+            IFornecedorService fornecedorService, 
+            IMapper mapper,
+            INotificador notificador) : base(notificador)
         {
+            _fornecedorRepository = fornecedorRepository;
+            _fornecedorService = fornecedorService;
+            _mapper = mapper;
         }
 
         [HttpGet]
         public async Task<IEnumerable<FornecedorViewModel>> ObterTodos()
         {
-            throw new NotImplementedException();
+            var result = _mapper.Map<IEnumerable<FornecedorViewModel>>(await _fornecedorRepository.ObterTodos());
+            return result;
         }
 
         [HttpGet("{id:guid}")]
         public async Task<ActionResult<FornecedorViewModel>> ObterPorId(Guid id)
         {
-            throw new NotImplementedException();
+            var fornecedorViewModel = await ObterFornecedorProdutosEndereco(id);
+
+            if (fornecedorViewModel == null) return NotFound();
+            return fornecedorViewModel;
         }
 
         [HttpPost]
         public async Task<ActionResult<FornecedorViewModel>> Adicionar(FornecedorViewModel fornecedorViewModel)
         {
-            throw new NotImplementedException();
+            if (!ModelState.IsValid) return CustomResponse(ModelState);
+
+            await _fornecedorService.Adicionar(_mapper.Map<Fornecedor>(fornecedorViewModel));
+            return CustomResponse(HttpStatusCode.Created, fornecedorViewModel);
         }
 
         [HttpPut("{id:guid}")]
         public async Task<ActionResult<FornecedorViewModel>> Atualizar(Guid id, FornecedorViewModel fornecedorViewModel)
         {
-            throw new NotImplementedException();
+            if (id != fornecedorViewModel.Id)
+            {
+                NotificarErro("Os ids informados não são iguais!");
+                return CustomResponse();
+            }
+
+            if (!ModelState.IsValid) return CustomResponse(ModelState);
+
+            await _fornecedorService.Atualizar(_mapper.Map<Fornecedor>(fornecedorViewModel));
+            return CustomResponse(HttpStatusCode.NoContent);
         }
 
         [HttpDelete("{id:guid}")]
         public async Task<ActionResult<FornecedorViewModel>> Excluir(Guid id)
         {
-            throw new NotImplementedException();
+            var fornecedor = await ObterFornecedorProdutosEndereco(id);
+
+            if (fornecedor == null) return NotFound();
+
+            await _fornecedorService.Remover(id);
+            return CustomResponse(HttpStatusCode.NoContent);
+        }
+
+        private async Task<FornecedorViewModel> ObterFornecedorProdutosEndereco(Guid id)
+        {
+            return _mapper.Map<FornecedorViewModel>(await _fornecedorRepository.ObterFornecedorProdutoEndereco(id));
         }
 
     }
